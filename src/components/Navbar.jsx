d0d2d0729358a763aae8fb2708cc333d4072f43d
solaiman-naderi <solaimannaderi00@gmail.com>
2023-10-23 03:51:22 -0400

    feat: added header section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../style";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 absolute top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
+        <Link
+          to={"/"}
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+          className="flex items-center gap-2"
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white font-bold cursor-pointer text-[18px]">
+            Solaiman Naderi &nbsp;{" "}
+            <span className="sm:block hidden">| portfolio</span>
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((item) => (
+            <li
+              key={item.id}
+              className={`${
+                active == item.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => {
+                setActive(item.title);
+              }}
+            >
+              <a href={`#${item.id}`}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => {
+              setToggle(!toggle);
+            }}
+          />
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex  justify-end items-start flex-col gap-4">
+              {navLinks.map((item) => (
+                <li
+                  key={item.id}
+                  className={`${
+                    active == item.title ? "text-white" : "text-secondary"
+                  } hover:text-white text-[16px] font-poppins font-medium cursor-pointer`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(item.title);
+                  }}
+                >
+                  <a href={`#${item.id}`}>{item.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
